93f2dc237f3cc2acd1905e210ba2357786ac5e21
George Yu <yudumpling@gmail.com>
2022-11-13 05:18:58 +0000

    Add custom colors and FA icons

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import 'flowbite'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faCartShopping, faScannerGun, faSliders, faHouse } from '@fortawesome/pro-duotone-svg-icons'
+import { faCartShopping, faScannerGun, faSliders, faHouse, faForkKnife, faTrees } from '@fortawesome/pro-duotone-svg-icons'
 import { faTree, faCoin } from '@fortawesome/pro-solid-svg-icons'
 
-library.add(faCartShopping, faScannerGun, faSliders, faHouse, faTree, faCoin)
+library.add(faCartShopping, faScannerGun, faSliders, faHouse, faForkKnife, faTrees, faTree, faCoin)
 
 import './index.css'
 import './assets/main.css'
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': { regular: "#88b04b", dark: "#476930", ultralight: "#f5f7ee" },
+        'complement': { regular: "#c9b374", dark: "#8c6f49" },
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin'),
