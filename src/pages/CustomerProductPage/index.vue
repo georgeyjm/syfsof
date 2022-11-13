798a1c117eb3126a71c37c49a0de1d8b45d3e9e0
George Yu <yudumpling@gmail.com>
2022-11-13 07:34:31 +0000

    Update customer shopping pages

--- src/pages/CustomerProductPage/index.vue
+++ src/pages/CustomerProductPage/index.vue
@@ -2,17 +2,17 @@
   <section class="p-10 mt-10" aria-label="Main page content">
     <h1 class="mb-10 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Product Details</h1>
 
-    <div class="flex">
-      <img class="w-100 h-100" src="https://picsum.photos/500" alt="">
-      <div class="ml-5">
-        <h2 class="mb-3 text-4xl font-bold tracking-normal leading-normal text-gray-900 lg:text-5xl dark:text-white">Watson & Watson Fresh Organic Irish Carrot</h2>
-        <p class="mb-5 text-4xl font-bold text-primary-regular">£200</p>
+    <div class="flex flex-wrap lg:flex-nowrap gap-5">
+      <img class="w-100 h-100 object-cover" src="https://picsum.photos/500" alt="">
+      <div class="">
+        <h2 class="mb-3 text-4xl font-bold tracking-normal  text-gray-900 lg:text-5xl dark:text-white">{{ prodName }}</h2>
+        <p class="mb-5 text-4xl font-bold text-primary-regular">£{{ price }}</p>
         <!-- <p class="text-xl">Sold By: <a href="#" class="font-bold">Some Random Dude</a></p> -->
-        <p class="mb-2 text-xl">Best Before: <strong>November 11, 2022</strong></p>
+        <p class="mb-2 text-xl">Best Before: <strong>{{ new Date(bestBefore * 1000).toLocaleDateString(undefined, {dateStyle: 'long'}) }} </strong></p>
         <p class="text-gray-500">
-          Origin: Lorem Ipsum Dolor Sit Amet
+          Origin: {{ origin }}
           <br>
-          Description: It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'.
+          Description: {{ description }}
         </p>
         <div class="mt-5 mb-2 text-lg text-primary-dark">
           <p>Buy & get</p>
@@ -35,6 +35,28 @@
     components: {
       ShopPageItemCard
     },
+    props: {
+      prodName: {
+        type: String,
+        default: 'Watson & Watson Fresh Organic Irish Carrot'
+      },
+      price: {
+        type: Number,
+        default: 200
+      },
+      bestBefore: {
+        type: Number,
+        default: 1668334392
+      },
+      origin: {
+        type: String,
+        default: 'Lorem Ipsum Dolor Sit Amet'
+      },
+      description: {
+        type: String,
+        default: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\'.'
+      }
+    }
   }
 </script>
 
--- src/pages/CustomerShopPage/index.vue
+++ src/pages/CustomerShopPage/index.vue
@@ -2,20 +2,20 @@
   <section class="p-10 mt-10" style="min-width: 0" aria-label="Main page content">
     <h1 class="mb-10 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Shop</h1>
 
-    <form>   
+    <form>
       <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
       <div class="relative">
           <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
             <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
           </div>
-          <input @input="searchFood" type="search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-          <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+          <input @input="searchFood" type="search" class="block p-4 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <!-- <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button> -->
       </div>
     </form>
 
     <div v-if="query !== ''">
       <div class="flex flex-wrap">
-        <ShopPageItemCard :title="food.foodInfo.name" v-for="food in recommendedFood" />
+        <ShopPageItemCard :title="food.foodInfo.name" :food="food" v-for="food in recommendedFood" />
       </div>
     </div>
 
@@ -41,16 +41,6 @@
       </div>
     </div>
 
-    <div class="mb-8">
-      <h2 class="mb-4 text-2xl font-bold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Shop Again</h2>
-      <div class="hide-scroll flex overflow-x-scroll">
-        <ShopPageItemCard title="Bread" />
-        <ShopPageItemCard title="Eggs" />
-        <ShopPageItemCard title="Maple Syrup" />
-        <ShopPageItemCard title="Bacon" />
-        <ShopPageItemCard title="Thomas's Incredibly Long and Unnecessary But Delicious Soup" />
-      </div>
-    </div>
   </section>
 </template>
 
@@ -59,8 +49,10 @@
   import { recommendFood } from '@/tokens.js'
   import ShopPageItemCard from '@/pages/CustomerShopPage/ShopPageItemCard.vue'
 
-  // thomas's eth
-  const sellers = ['0xfeB23A728C560e7D186523eC2C90298f1eE1Ea1a']
+  const sellers = [
+    '0xfeB23A728C560e7D186523eC2C90298f1eE1Ea1a',
+    '0xdCaF5C965bE2282528CD35bF8D36495Bd9609236'
+  ]
 
   export default {
     name: 'CustomerShopPage',
